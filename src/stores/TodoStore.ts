4b42ef8b5d67c233e91955bb673994f7b06d2ad0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-04-16 17:25:08 -0400

    Begin to add the TodoStore

---
+++ src/stores/TodoStore.ts
@@ -0,0 +1,22 @@
+export interface Todo {
+    id: number;
+    title: string;
+    isDone: boolean;
+}
+
+class TodoStore {
+
+    list: Todo[] = [];
+    add(title: string) {
+        if (title.length < 3) {
+            return;
+        }
+        this.list.push({ id: Date.now(), isDone: false, title });
+    }
+    toggle(todo: Todo) {
+        todo.isDone = !todo.isDone
+    }
+    remove(todo: Todo) {
+        this.list = this.list.filter(t => t.id !== todo.id);
+    }
+}
