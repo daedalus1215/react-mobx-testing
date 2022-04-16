af7e865fffebbc8f4e7b94539549791a055c9c48
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-04-16 18:04:31 -0400

    Update App.tsx's css import

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import TodoStore from './stores/TodoStore';
 import TodoInput from './todo/todoInput/TodoInput';
 import TodoList from './todo/todoList/TodoList';
+import './App.css';
 
 const todos = new TodoStore();
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <TodoInput todos={todos} />
       <TodoList todos={todos} />
     </div>
