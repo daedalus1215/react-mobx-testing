54120a22549eee96ef0b907c42773b68e8f96f48
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-04-17 09:52:18 -0400

    useState and observable, before using the shorthand.

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,31 @@ import TodoInput from './todo/todoInput/TodoInput';
 import TodoList from './todo/todoList/TodoList';
 import './App.css';
 import styles from './App.module.css';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-  const [todosVisible, setTodosVisible] = useState(true);
+  const [appUI] = useState(() => observable({
+    todosVisible: true,
+    toggleTodoVisibility() {
+      this.todosVisible = !this.todosVisible;
+    }
+  }));
 
-  const handleClick = () => {
-    setTodosVisible(state => !state);
-  };
+  const handleClick = () => appUI.toggleTodoVisibility();
 
   return (
     <div className="app">
       <TodoInput />
       <div className={styles['todo-list-wrapper']}>
         <h2 onClick={handleClick}>
-          <span>{todosVisible ? '-' : '+'}</span>
+          <span>{appUI.todosVisible ? '-' : '+'}</span>
           Todos
         </h2>
-        {todosVisible && <TodoList />}
+        {appUI.todosVisible && <TodoList />}
       </div>
     </div>
   );
 }
 
-export default App;
+export default observer(App);
