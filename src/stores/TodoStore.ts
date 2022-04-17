ed3efa03b0d74c4d9a8d3ee7a110d3626098f763
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-04-16 21:18:11 -0400

    Experimenting with decorators.

--- src/stores/TodoStore.ts
+++ src/stores/TodoStore.ts
@@ -6,26 +6,30 @@ export interface Todo {
 }
 
 class TodoStore {
+    @observable
     list: Todo[] = [];
 
     constructor() {
-        makeObservable(this, {
-            list: observable,
-            add: action,
-            toggle: action,
-            remove: action,
-        });
+        // makeObservable(this, {
+        //     list: observable,
+        //     add: action,
+        //     toggle: action,
+        //     remove: action,
+        // });
+        makeObservable(this)
     }
-
+    @action
     add(title: string) {
         if (title.length < 3) {
             return;
         }
         this.list.push({ id: Date.now(), isDone: false, title });
     }
+    @action
     toggle(todo: Todo) {
         todo.isDone = !todo.isDone
     }
+    @action
     remove(todo: Todo) {
         this.list = this.list.filter(t => t.id !== todo.id);
     }
