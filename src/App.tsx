bf0e16cbb1b001e24b7a9878fce6bde37eb4443d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-04-17 09:46:19 -0400

    Toggle Todo List

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoStore from './stores/TodoStore';
 import TodoInput from './todo/todoInput/TodoInput';
 import TodoList from './todo/todoList/TodoList';
 import './App.css';
-
-const todos = TodoStore();
+import styles from './App.module.css';
 
 function App() {
+  const [todosVisible, setTodosVisible] = useState(true);
+
+  const handleClick = () => {
+    setTodosVisible(state => !state);
+  };
+
   return (
     <div className="app">
       <TodoInput />
-      <TodoList/>
+      <div className={styles['todo-list-wrapper']}>
+        <h2 onClick={handleClick}>
+          <span>{todosVisible ? '-' : '+'}</span>
+          Todos
+        </h2>
+        {todosVisible && <TodoList />}
+      </div>
     </div>
   );
 }
